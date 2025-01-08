d1c3a0dd5cebc52c6c26fe407abfe414445d905f
Kania <kania.praca@wp.pl>
2025-01-08 16:10:09 +0100

    Fix Kafka producer configuration

---
+++ src/main/java/com/christianj98/online_store/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.christianj98.online_store.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic ordersTopic() {
+        return TopicBuilder.name("orders")
+                .partitions(5)
+                .replicas(1)
+                .build();
+    }
+}
--- src/main/java/com/christianj98/online_store/dto/CreateOrderDto.java
+++ src/main/java/com/christianj98/online_store/dto/CreateOrderDto.java
@@ -3,6 +3,6 @@ package com.christianj98.online_store.dto;
 public record CreateOrderDto(
         Integer orderId,
         String product,
-        String quantity
+        Integer quantity
 ) {
 }
