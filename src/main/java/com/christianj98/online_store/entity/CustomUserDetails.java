51dea4aef7398517a05bc537c945c7a80de5ae4b
Kania <kania.praca@wp.pl>
2025-01-10 21:16:16 +0100

    Create custom user details implementing UserDetails interface

---
+++ src/main/java/com/christianj98/online_store/entity/CustomUserDetails.java
@@ -0,0 +1,34 @@
+package com.christianj98.online_store.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
