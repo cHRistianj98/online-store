737f65cc281dfb1e06d2181965ad7dd8abf76ac8
Kania <kania.praca@wp.pl>
2025-03-21 23:07:31 +0100

    Remove space

--- src/main/java/com/christianj98/online_store/OnlineStoreApplication.java
+++ src/main/java/com/christianj98/online_store/OnlineStoreApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class OnlineStoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStoreApplication.class, args);
 	}
